a465a4bc067e7c89058c3ac04eb01c34b19ff5a8
Harja <rachmadsuharja123@gmail.com>
2023-10-10 09:56:48 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -137,4 +137,3 @@ Route::get('/about-laravel', function () {
     return view('welcome');
 });
 Auth::routes();
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
